32081269f8096ce5ff5fd6e8132cba547e0ba8a8
Nikhil <nikhilmishra4713@gmail.com>
2026-04-09 03:34:55 +0530

    few changes

--- tweetheadq/tweetheadq/urls.py
+++ tweetheadq/tweetheadq/urls.py
@@ -19,12 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from tweet import views  # import your app views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tweet/", include("tweet.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    # Homepage route
-    path("", TemplateView.as_view(template_name="index.html"), name="home"),
+    # Homepage shows tweet feed
+    path("", views.tweet_list, name="home"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
