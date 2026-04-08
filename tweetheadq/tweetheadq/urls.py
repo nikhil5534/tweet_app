0d8878a59313300445bba6928d696dca2ec67d41
Nikhil <nikhilmishra4713@gmail.com>
2026-04-09 03:28:30 +0530

    few changes

--- tweetheadq/tweetheadq/urls.py
+++ tweetheadq/tweetheadq/urls.py
@@ -19,9 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("tweet/", include('tweet.urls')),
-    path("accounts/", include('django.contrib.auth.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path("tweet/", include("tweet.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Homepage route
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
