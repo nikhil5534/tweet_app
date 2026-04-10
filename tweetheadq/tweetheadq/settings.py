d0d5451ad8ed03ac208eae22d0463cc06f7a2302
Nikhil <nikhilmishra4713@gmail.com>
2026-04-11 01:24:17 +0530

    remove whitenoise

--- tweetheadq/tweetheadq/settings.py
+++ tweetheadq/tweetheadq/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
 "django.middleware.security.SecurityMiddleware",
-"whitenoise.middleware.WhiteNoiseMiddleware",
 "django.contrib.sessions.middleware.SessionMiddleware",
 "django.middleware.common.CommonMiddleware",
 "django.middleware.csrf.CsrfViewMiddleware",
@@ -89,8 +88,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 
 # CLOUDINARY CONFIG
 
