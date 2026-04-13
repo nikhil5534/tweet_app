7d8f81b15d546e22c1257ea9dcde24bcfd93f4e1
Nikhil <nikhilmishra4713@gmail.com>
2026-04-13 18:14:07 +0530

    better Ui search try 2

--- tweetheadq/tweetheadq/settings.py
+++ tweetheadq/tweetheadq/settings.py
@@ -63,32 +63,32 @@ WSGI_APPLICATION = "tweetheadq.wsgi.application"
 
 # DATABASE (Supabase)
 
-# DATABASES = {
-# "default": dj_database_url.config(
-# default=os.environ.get("DATABASE_URL"),
-# conn_max_age=600,
-# ssl_require=True
-# )
-# }
+DATABASES = {
+"default": dj_database_url.config(
+default=os.environ.get("DATABASE_URL"),
+conn_max_age=600,
+ssl_require=True
+)
+}
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+# DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    # Fallback for local development
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
+# if DATABASE_URL:
+#     DATABASES = {
+#         "default": dj_database_url.parse(
+#             DATABASE_URL,
+#             conn_max_age=600,
+#             ssl_require=True
+#         )
+#     }
+# else:
+#     # Fallback for local development
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#         }
+#     }
 
 # PASSWORD VALIDATION
 
