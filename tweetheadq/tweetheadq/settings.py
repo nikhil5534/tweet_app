c6046dedbf5fca68c391f1937ff3d23dc8a1672c
Nikhil <nikhilmishra4713@gmail.com>
2026-04-13 20:15:28 +0530

    after python reverse changes

--- tweetheadq/tweetheadq/settings.py
+++ tweetheadq/tweetheadq/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-secret")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-# DEBUG = True
+
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app"]
 
 # APPS
@@ -67,25 +67,6 @@ conn_max_age=600,
 ssl_require=True
 )
 }
-
-# DATABASE_URL = os.environ.get("DATABASE_URL")
-
-# if DATABASE_URL:
-#     DATABASES = {
-#         "default": dj_database_url.parse(
-#             DATABASE_URL,
-#             conn_max_age=600,
-#             ssl_require=True
-#         )
-#     }
-# else:
-#     # Fallback for local development
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#         }
-#     }
 
 # PASSWORD VALIDATION
 
